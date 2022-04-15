f1385a6f39f122a71eebf74fd607c25f7195241c
ZakAADDI <86986632+ZakAADDI@users.noreply.github.com>
2022-04-15 03:54:48 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$("#charg").load("accueil.html");
+
+function contact(){
+    $("#charg").load("contact.html");
+}
+
+$("#annecy").on('click', function () {
+
+    $("#charg").load("accueil.html");
+})
+
+function submit(){
+    $("#charg").load("submit.html");
+}
+
